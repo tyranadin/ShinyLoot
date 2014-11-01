fef65fcaacc4e79896471273b58ff576c71ff655
tyranadin <asmodaie@gmail.com>
2014-11-01 14:43:53 -0700

    ShinyLoot Base

---
+++ README.md
@@ -0,0 +1,4 @@
+ShinyLoot
+Author: Tyranadin of Moon Guard (US)
+
+Automatically highlights the item with the highest vendor value when completing a quest.
---
+++ ShinyLoot/ShinyLoot.lua
@@ -0,0 +1,40 @@
+--[[ 
+	ShinyLoot, by Tyranadin (US - Moon Guard)
+]]
+
+local ShinyLoot,sl = ...;
+
+local GetItemInfo = GetItemInfo;
+local GetNumQuestChoices = GetNumQuestChoices;
+local GetQuestItemLink = GetQuestItemLink;
+local select = select;
+
+sl.frame = CreateFrame("Frame");
+sl.frame:SetScript("OnEvent",function(self,event,...)
+	if QuestFrameRewardPanel:IsShown() then
+		sl.reward();
+	end
+end);
+sl.frame:RegisterEvent("QUEST_COMPLETE");
+sl.frame:RegisterEvent("QUEST_ITEM_UPDATE");
+sl.frame:RegisterEvent("GET_ITEM_INFO_RECEIVED");
+
+function sl.reward()
+	local index, gold = 0, 0;
+
+	for i=1, GetNumQuestChoices() do
+		local link = GetQuestItemLink("choice", i)
+		if ( link == nil ) then
+			return
+		end
+		local g = select(11, GetItemInfo(link))
+		if g > gold then
+			gold = g
+			index = i
+		end
+	end
+
+	if index then
+		QuestInfoItem_OnClick(_G["QuestInfoRewardsFrameQuestInfoItem"..index]);
+	end
+end
